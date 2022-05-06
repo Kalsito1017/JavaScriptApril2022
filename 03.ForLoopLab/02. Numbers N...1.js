a97cab69b75463eba64471344c3868f309e58b41
Kalsito1017 <kurabiika34@abv.bg>
2022-05-06 21:01:37 +0300

    Added Exercises with for loop

---
+++ 03.ForLoopLab/02. Numbers N...1.js
@@ -0,0 +1,6 @@
+function NumberrsN1(input){
+    let n = Number(input[0]);
+    for(let i = n; i>0;i--){
+        console.log(i);
+    }
+}
---
+++ 03.ForLoopLab/03. Numbers 1...N with Step 3.js
@@ -0,0 +1,6 @@
+function NumberrsN1withStep3(input){
+    let number = Number(input[0]);
+    for(let i = 1; i <= number; i +=3){
+        console.log(i);
+    }
+}
