17261eda50fb8cc312afbba84d9ba0d966046443
Kalsito1017 <kurabiika34@abv.bg>
2022-05-18 23:57:08 +0300

    Added some Empty Exercises

--- 04.WhileLoopLab/01. Read Text.js
+++ 04.WhileLoopLab/01. Read Text.js
@@ -1,7 +1,12 @@
-function ReadingText(input){
-    let text = String(input[0]);
-    while(text !== "Stop"){
-        console.log(text);
-        text = String(input[1]);
+function ReadText(input)
+let str = String(input[0]);
+let index = 0;
+while(true){
+    let str = input[index];
+    index++;
+    if(str === "Stop"){
+        break;
     }
+    console.log(str);
+    
 }
